0669af2e3e997e3e0e0ebd7a3cc77bfc3fec14d7
Darko Micic <darko@micic.ch>
2019-09-12 08:27:19 +0200

    useBusState subscriber option (#36)
    useBusState subscriber option

--- src/useBusState.ts
+++ src/useBusState.ts
@@ -1,6 +1,20 @@
 import { useState, useLayoutEffect } from "react";
 import { useBus } from "./react";
-import { EventCreatorFn, BusEvent } from "./types";
+import { EventCreatorFn, BusEvent, SubscribeWithPayloadDispatchFn } from './types';
+
+const useStateCreator = <E extends BusEvent = BusEvent>(
+  subscriber: SubscribeWithPayloadDispatchFn<E>
+) => (
+  initState: E["payload"] | (() => E["payload"])
+) => {
+    const bus = useBus();
+
+    const [state, dispatch] = useState<E["payload"]>(initState);
+
+    useLayoutEffect(() => subscriber(dispatch, bus), [dispatch, bus]);
+
+    return state;
+  };
 
 export function useBusState<E extends BusEvent = BusEvent>(
   initState: E["payload"] | undefined,
@@ -8,16 +22,18 @@ export function useBusState<E extends BusEvent = BusEvent>(
 ): E["payload"] {
   const bus = useBus();
 
-  const [state, dispatch] = useState<E["payload"]>(initState);
-
-  useLayoutEffect(() => {
-    const unsubscribe = bus.subscribe<E>(event, (v: E) => {
-      dispatch(v.payload);
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [dispatch, bus]);
-
-  return state;
+  const useState = useStateCreator((dispatch, bus) => {
+    return bus.subscribe(event, (ev: E) => dispatch(ev.payload))
+  });
+  return useState(initState);
 }
+
+type UseBusStateOptions<E extends BusEvent> = {
+  subscriber: SubscribeWithPayloadDispatchFn<E>;
+};
+
+useBusState.configure = <E extends BusEvent = BusEvent>(
+  options: UseBusStateOptions<E>
+) => {
+  return useStateCreator(options.subscriber);
+};
