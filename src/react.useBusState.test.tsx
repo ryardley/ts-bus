546aa0532294a3d4267fe181c25861761d6021f9
Rudi Yardley <contact@rudiyardley.com>
2020-02-22 16:45:36 +1100

    Move useBusState to use a tuple

--- src/react.useBusState.test.tsx
+++ src/react.useBusState.test.tsx
@@ -4,11 +4,6 @@ import { createEventDefinition } from "./EventBus";
 import { BusProvider, stateSubscriber, useBus, useBusState } from "./react";
 import { bus, mockEventBus, wrapper } from "./testhelpers";
 
-it("should provide a bus", () => {
-  const { result } = renderHook(() => useBus(), { wrapper });
-  expect(result.current).toBe(bus);
-});
-
 it("should not subscribe without unsubscribing (useBusState)", () => {
   const mockBus = mockEventBus();
   const incrementEvent = createEventDefinition<number>()("increment");
@@ -31,7 +26,7 @@ it("should update state (options configuration)", () => {
 
   const { result } = renderHook(
     () =>
-      useBusState.configure({
+      useBusState.configure(incrementEvent, {
         subscriber: (dispatch, bus) => {
           return bus.subscribe("counter.**", v => dispatch(v.payload));
         }
@@ -41,22 +36,33 @@ it("should update state (options configuration)", () => {
     }
   );
 
-  expect(result.current).toBe(0);
+  expect(result.current[0]).toBe(0);
 
   act(() => {
     bus.publish(incrementEvent(1));
   });
 
-  expect(result.current).toBe(1);
+  expect(result.current[0]).toBe(1);
+
+  act(() => {
+    result.current[1](20); // set value to 20
+  });
+
+  expect(result.current[0]).toBe(20);
 });
 
 it("should update state by subscribing to multiple events", () => {
   const positiveNumberEvent = createEventDefinition<number>()("positive");
   const negativeNumberEvent = createEventDefinition<number>()("negative");
 
+  // We need an event that will be fired
+  // when we use the 'dispatch' function
+  // returned form the useState() fn
+  const posNeg = createEventDefinition<number>()("posNeg");
+
   const { result } = renderHook(
     () =>
-      useBusState.configure({
+      useBusState.configure(posNeg, {
         subscriber: stateSubscriber("positive", "negative")
       })(0),
     {
@@ -64,13 +70,13 @@ it("should update state by subscribing to multiple events", () => {
     }
   );
 
-  expect(result.current).toBe(0);
+  expect(result.current[0]).toBe(0);
 
   act(() => bus.publish(positiveNumberEvent(15)));
-  expect(result.current).toBe(15);
+  expect(result.current[0]).toBe(15);
 
   act(() => bus.publish(negativeNumberEvent(-5)));
-  expect(result.current).toBe(-5);
+  expect(result.current[0]).toBe(-5);
 });
 
 it("should update state", () => {
@@ -80,11 +86,50 @@ it("should update state", () => {
     wrapper
   });
 
-  expect(result.current).toBe(0);
+  expect(result.current[0]).toBe(0);
 
   act(() => {
     bus.publish(incrementEvent(1));
   });
 
-  expect(result.current).toBe(1);
+  expect(result.current[0]).toBe(1);
+});
+
+describe("when the dispatchEvent is not the same as the subscribe event", () => {
+  it("should set the dispatch event only and be able to subscribe to other events", () => {
+    const one = createEventDefinition<number>()("one");
+    const two = createEventDefinition<number>()("two");
+    const three = createEventDefinition<number>()("three");
+
+    const { result } = renderHook(
+      () =>
+        useBusState.configure(three, {
+          subscriber: stateSubscriber("one", "two")
+        })(0),
+      {
+        wrapper
+      }
+    );
+
+    expect(result.current[0]).toBe(0);
+
+    act(() => {
+      bus.publish(one(1));
+    });
+
+    expect(result.current[0]).toBe(1);
+
+    act(() => {
+      bus.publish(two(2));
+    });
+
+    expect(result.current[0]).toBe(2);
+
+    act(() => {
+      result.current[1](3);
+    });
+
+    // Remains 2 because subscription does not include "three" event
+    expect(result.current[0]).toBe(2);
+  });
 });
